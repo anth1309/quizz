ab692e5f241371f3ae95a0bb72187ba0eb3b1f98
Anthony CHAMBON <acdf-ac.cdf@hotmail.fr>
2022-12-07 13:19:06 +0100

    manque info /rep fausse

--- index.js
+++ index.js
@@ -30,91 +30,68 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   //Validation nb rep juste ou fausse
-  {
-    var boutonValidation = document.getElementById("validation");
-    var arrayRep = [
-      "question1",
-      "question2",
-      "question3",
-      "question4",
-      "question5",
-      "question6",
-      "question7",
-      "question8",
-      "question9",
-      "question10",
-    ];
-    boutonValidation.addEventListener("click", function () {
-      const trueanswer = [1, 2, 2, 3, 3, 2, 3, 3, 3, 1];
-      const container = document.getElementsByClassName("container");
-      const espaceMessage = document.getElementById("resultatNote");
-      var reponse1Eleve = document.querySelectorAll("input:checked");
-      let note = 0;
-      for (i = 0; i < 10; i++) {
-        var reponseEleve = reponse1Eleve.item(i);
-        if (reponseEleve.value == trueanswer[i]) {
-          console.log("question " + arrayRep[i] + "   ok");
-          note = note + 1;
+  const answer = document.querySelector("answer");
+  var boutonValidation = document.getElementById("validation");
 
-          //document.getElementsByClassName("answer").classList.add("juste");
-          espaceMessage.innerHTML = `Vous avez obtenu la note de ${note}/10`;
-        } else {
-          console.log("question " + arrayRep[i] + "   nmauvais");
-        }
+  boutonValidation.addEventListener("click", function () {
+    const trueanswer = [1, 2, 2, 3, 3, 2, 3, 3, 3, 1];
+    const espaceMessage = document.getElementById("resultatNote");
+    var reponse1Eleve = document.querySelectorAll("input:checked");
+    const visibleCorrection = document.querySelectorAll("section");
+    //visibleCorrection.classList.add("visibleCorrection");
+    //const reponseFausses = [];
+    let note = 0;
+    for (i = 0; i < 10; i++) {
+      var reponseEleve = reponse1Eleve.item(i);
+
+      if (reponseEleve.value == trueanswer[i]) {
+        note = note + 1;
+        reponseEleve.classList.add("juste");
+        // console.log(reponseEleve.name);
+
+        //console.log(section.id);
+        //reponseEleve.classList.add("visibleCorrection");
+
+        espaceMessage.innerHTML = `Vous avez obtenu la note de ${note}/10`;
+        window.scrollTo(00, 00);
+      } else {
+        reponseEleve.classList.add("fausse");
+        //reponseFausses.push(reponseEleve.name);
       }
-    });
 
-    //activation boutton soumettre
+      //for (let reponseFausse of reponseFausses) {
+      //console.log((id = reponseFausse));
+      //}
+    }
+  });
+  //activation boutton soumettre
 
-    var boutonValidation = document.getElementById("validation");
-    var array = [
-      "question1",
-      "question2",
-      "question3",
-      "question4",
-      "question5",
-      "question6",
-      "question7",
-      "question8",
-      "question9",
-      "question10",
-    ];
-    let compteur = 0;
-    var nombreReponse = document.querySelectorAll("section input");
-    nombreReponse.forEach((e) => {
-      e.addEventListener("click", function () {
-        // alert("ppyt  " + e.name);
-        for (i = 0; i < 10; i++) {
-          if (e.name === array[i]) {
-            delete array[i];
-            compteur = compteur + 1;
-          }
-          if (compteur == 10) {
-            boutonValidation.disabled = false;
-          }
-        }
-      });
-    });
-  }
-});
-
-for (i = 0; i < 10; i++) {
-  var nombreTotalReponse = nombreReponse;
-  nombreTotalReponse.forEach((e) => {
+  var boutonValidation = document.getElementById("validation");
+  var array = [
+    "question1",
+    "question2",
+    "question3",
+    "question4",
+    "question5",
+    "question6",
+    "question7",
+    "question8",
+    "question9",
+    "question10",
+  ];
+  let compteur = 0;
+  var nombreReponse = document.querySelectorAll("section input");
+  nombreReponse.forEach((e) => {
     e.addEventListener("click", function () {
-      nombreTotalReponse = nombreTotalReponse + 1;
-      console.log("nb " + nombreTotalReponse.value);
-      console.log("ppp   " + questionEleve);
-      if (nombreTotalReponse == 10) {
-        boutonValidation.disabled = false;
+      for (i = 0; i < 10; i++) {
+        if (e.name === array[i]) {
+          delete array[i];
+          compteur = compteur + 1;
+        }
+        if (compteur == 10) {
+          boutonValidation.disabled = false;
+        }
       }
     });
   });
-}
-// });
-//  reponsesq2.forEach((e) => {
-//    e.addEventListener("click", function () {
-//      q2 = true;
-//      verifquestionnaire();
-//    });
-//  });
+});
